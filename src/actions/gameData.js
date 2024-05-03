febbec43fe37be2557a2bd0e15fac99ae197e9f5
Archey <Archey.bald6@gmail.com>
2024-05-03 01:48:22 -0600

    Update gameData.js

--- src/actions/gameData.js
+++ src/actions/gameData.js
@@ -73,7 +73,7 @@ export const fetchItems = (...items) => (dispatch, getState) => {
   const chunk = 100;
   for (let i = 0, j = itemIds.length; i < j; i += chunk) {
     const itemIdStr = itemIds.slice(i, i + chunk).join(',');
-
+	console.log(substitute(config.endpoints.service.item, { itemIds: itemIdStr }));
     xhr.get(substitute(config.endpoints.service.item, { itemIds: itemIdStr }))
     .then(({ data }) => {
       dispatch({ type: DATA_ITEM, data: arrayToMap(data) });
