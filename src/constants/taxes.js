2e650b93b78fa6631463451dc38bb7ee4f07db80
Archey <Archey.bald6@gmail.com>
2024-05-05 02:08:50 -0600

    Update taxes.js

--- src/constants/taxes.js
+++ src/constants/taxes.js
@@ -6,8 +6,8 @@ export const TAX_HOSTILE = 'TAX_HOSTILE';
 export const BUILDING_NAME_REGEX = /^(?:(?:Bound )?Design: )?(?:Full[ -]Kit: )?((?:(?! Design| Kit).)*)/;
 export const BUILDING_SIZE_REGEX = /(\d+) ?m x \d+ ?m/;
 export const BUILDING_TYPE_REGEX = /(?:Classification|Category): ([\w ]+\w)/;
-export const BUILDING_DEPOSIT_REGEX = /Security Deposit: [^\d]+(\d+)/;
-export const BUILDING_TAXES_REGEX = /Taxes: [^\d]+(\d+)/;
+export const BUILDING_DEPOSIT_REGEX = /Security Deposit:[^\d]+(\d+)/;
+export const BUILDING_TAXES_REGEX = /Taxes:[^\d]+(\d+)/;
 
 export const HEAVY_TAX_RATE = Object.freeze([
   0,
